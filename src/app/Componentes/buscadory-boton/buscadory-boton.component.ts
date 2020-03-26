5c1680b52d142298cec5b14323366b4b847c7738
Adrian Fuentes <adrian.fuentez11@gmail.com>
2020-03-25 18:11:44 -0600

    Avance 7 (Validacion Checkbox)

--- src/app/Componentes/buscadory-boton/buscadory-boton.component.ts
+++ src/app/Componentes/buscadory-boton/buscadory-boton.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DatosService } from 'src/app/services/datos.service';
 import { FormGroup , FormBuilder, Validators, FormControl} from "@angular/forms";
-
+import {requireCheckboxesToBeCheckedValidator} from "./require-checkboxes-to-be-checked.validator"
 
 @Component({
   selector: 'app-buscadory-boton',
@@ -25,6 +25,8 @@ checado5 = false;
 checado6 = false;
 checado7 = false;
 */
+chequeos:any;
+chequeos2:any;
 bloqueo1 = false;
 bloqueo2 = false;
 bloqueo3 = false;
@@ -33,33 +35,57 @@ bloqueo4 = false;
 bloq1 = false;
 bloq2 = false;
 bloq3 = false;
-form:FormGroup;
-
+form3:FormGroup;
+mensaje_name:any;
 
   constructor(public servicio: DatosService,private fb:FormBuilder) { 
-    this.form = fb.group({
+    this.mensaje_name = {
+      'name': [
+        { type: 'required', message: 'El nombre es Requerido*' },
+        { type: 'minlength', message: 'Minimo 3 letras*'},
+        { type: 'pattern', message: 'Solo se aceptan letras*' }
+      ]}
+
+
+  /*  this.form3 = fb.group({
       name: ['',Validators.compose([
         Validators.required,Validators.minLength(3),
-            Validators.pattern('[A-Za-z]*') ])],
-        chequeos: new FormGroup({
-          checado1: new FormControl(false),
-          checado2: new FormControl(false),
-          checado3: new FormControl(false),
-          checado4: new FormControl(false)
-        }),
-        chequeos2: new FormGroup({
-          checado5: new FormControl(false),
-          checado6: new FormControl(false),
-          checado7: new FormControl(false)
-        })
-
-    })
+            Validators.pattern('[A-Za-z]*') ])]
+            })
+*/
+//se crea un formgroup para las validaciones
+            this.form3 = new FormGroup({
+              //se secciona por validacion (esta es del name)
+              name: new FormControl('',[Validators.compose([
+                Validators.required,Validators.minLength(3),
+            Validators.pattern('[A-Za-z]*')]) ]),
+                //Seccion del primer grupo de checks
+            chequeos:  new FormGroup({
+            checado1: new FormControl(false),
+            checado2: new FormControl(false),
+            checado3: new FormControl(false),
+            checado4: new FormControl(false),
+            //funcion que llama un .ts que hace la validacion de un grupo
+            },requireCheckboxesToBeCheckedValidator()),
+            //seccion del segundo grupo de checks
+            chequeos2: new FormGroup({
+            checado5: new FormControl(false),
+            checado6: new FormControl(false),
+            checado7: new FormControl(false)
+            //misma funcion para comprobar comoa arriba xD
+          },requireCheckboxesToBeCheckedValidator())
+            })
+    
   }
+  
  
-Guardar(){
+Guardar(na:any){
+  console.log("avr");
+  
+console.log(na.value);
 
   this.id = this.servicio.pasaP.length +1;
-this.servicio.pasaP.push({id:this.id , nande:this.name, Fecha:'GDFN123654IO7'});
+this.servicio.pasaP.push({id:this.id , nande: na.value, Fecha:'GDFN123654IO7'});
 console.log(this.Datitos);
 this.servicio.paso();
 }
@@ -71,6 +97,10 @@ this.showModall = false;
 }
 RegresarD(){
 
+}
+
+submitForm(formData: any): void {
+  this.form3.reset();
 }
   ngOnInit(): void {
   }
