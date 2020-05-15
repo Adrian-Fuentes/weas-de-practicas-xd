5bf9684e55c66b2e044daa84378ee5af9771fa6d
Adrian Fuentes <adrian.fuentez11@gmail.com>
2020-05-15 18:29:36 -0500

    Avance Diseño 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TesteoPractica';
-  showFiller = false;
+  MuestraContenido = false;
   Confi = false;
   oculta = false;
   Clientes = false;
@@ -15,4 +15,57 @@ export class AppComponent {
   Teso = false;
   Dispo = false;
   Gestion = false;
+
+
+  
+  ActivaConfig(){
+    this.Clientes = false;
+    this.Credito = false;
+    this.Teso = false;
+    this.Dispo = false;
+    this.Gestion = false;
+  }
+  ActivaClientes(){
+    this.Confi = false;
+    this.Credito = false;
+    this.Teso = false;
+    this.Dispo = false;
+    this.Gestion = false;
+   //Dependiendo de la funcionalidad se utilizará:-----> this.MuestraContenido = false;
+  }
+  ActivaCredito(){
+    this.Clientes = false;
+    this.Confi = false;
+    this.Teso = false;
+    this.Dispo = false;
+    this.Gestion = false;
+  
+  }
+  ActivaTeso(){
+    this.Clientes = false;
+    this.Credito = false;
+    this.Confi = false;
+    this.Dispo = false;
+    this.Gestion = false;
+    
+  }
+  ActivaDispo(){
+    this.Clientes = false;
+    this.Credito = false;
+    this.Teso = false;
+    this.Confi = false;
+    this.Gestion = false;
+    
+  }
+  ActivaGestion(){
+    this.Clientes = false;
+    this.Credito = false;
+    this.Teso = false;
+    this.Dispo = false;
+    this.Confi = false;
+    
+  }
+
+
+
 }
